06ad3def42f5ecf247afb5b68a3885b6a33ab0c3
aditya-tripathee <adityatripatheee@gmail.com>
2026-03-26 20:20:07 +0530

    initialize backend server

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,28 @@
+import cookieParser from "cookie-parser";
 import express from "express";
+import { connectDB } from "./config/db.js";
+import dotenv from "dotenv";
+dotenv.config();
+
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT;
+
+
+// middlewares
+app.use(express.json());
+app.use(cookieParser());
+
+
+// routes 
+app.get("/",(req,res)=>{
+    res.send("Home Page ")
+})
+
 
 
 // server 
 app.listen(PORT, () => {
   console.log(`Server is listening on PORT ${PORT}`);
+  connectDB();
 });
