47dc9cb56dba8f478ca51c63198e22d57577286b
aditya-tripathee <adityatripatheee@gmail.com>
2026-03-27 11:25:00 +0530

    auth middlewares setup

---
+++ backend/middlewares/auth.middleware.js
@@ -0,0 +1,26 @@
+import jwt from "jsonwebtoken";
+import User from "../models/auth.model";
+
+
+export const authMiddleware = async(req,res,next)=>{
+    try {
+        const token = req.cookies.token;
+        if(!token){
+            return res.status(400).json({message:"Unauthorized"})
+        }
+
+        const decoded = jwt.verify(token,process.env.JWT_SECRET);
+        const user = await User.findById(decoded.userId).select("-password");
+        if(!user){
+            return res.status(404).json({message:"User not found"})
+        }
+        req.user = user;
+        next();
+        
+
+    } catch (error) {
+        console.error("Auth middleware error ",error);
+        return res.status(500).json({message:"Internal server error"});
+    }
+}
+
