ac02a77263ae25255ecf046abe737b532ebab631
aditya-tripathee <adityatripatheee@gmail.com>
2026-03-28 16:13:14 +0530

    favourites route

--- backend/routes/favourite.route.js
+++ backend/routes/favourite.route.js
@@ -1,9 +1,14 @@
 import express from "express";
+import { addFavourite, getFavourites, removeFavourite } from "../controllers/favourite.controller.js";
+import { authMiddleware } from "../middlewares/auth.middleware.js";
 
 
 const favRouter = express.Router();
 
-favRouter.post();
+favRouter.post("/:id",authMiddleware,addFavourite);
+favRouter.get("/",authMiddleware,getFavourites);
+favRouter.delete("/:id",authMiddleware,removeFavourite);
+
 
 
 export default favRouter;
