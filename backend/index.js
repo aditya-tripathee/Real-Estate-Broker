5b00ef71610c62fcb531d784196ff284315bb1f1
aditya-tripathee <adityatripatheee@gmail.com>
2026-03-28 16:14:47 +0530

    added routes

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,11 @@ import cookieParser from "cookie-parser";
 import express from "express";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
+import authRouter from "./routes/auth.route.js";
+import propertyRouter from "./routes/property.route.js";
 dotenv.config();
+import "./config/cloudinary.js";
+import favRouter from "./routes/favourite.route.js";
 
 
 const app = express();
@@ -12,12 +16,20 @@ const PORT = process.env.PORT;
 // middlewares
 app.use(express.json());
 app.use(cookieParser());
+app.use(express.urlencoded({extended:true}));
 
 
-// routes 
-app.get("/",(req,res)=>{
-    res.send("Home Page ")
-})
+
+
+// // routes 
+// app.get("/",(req,res)=>{
+//     res.send("Home Page ")
+// })
+
+// router 
+app.use("/api/auth/",authRouter);
+app.use("/api/property",propertyRouter);
+app.use("/api/fav",favRouter);
 
 
 
