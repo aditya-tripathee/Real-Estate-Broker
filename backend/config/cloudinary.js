bf23c55bd71be10b94a0dc252ae11152689e4caf
aditya-tripathee <adityatripatheee@gmail.com>
2026-03-28 16:03:56 +0530

    cloudinary setup with multer

--- backend/config/cloudinary.js
+++ backend/config/cloudinary.js
@@ -1,22 +1,29 @@
-import multer from "multer"
-import {CloudinaryStorage} from "multer-storage-cloudinary";
-import {v2 as cloudinary} from "cloudinary";
+import multer from "multer";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
+import { v2 as cloudinary } from "cloudinary";
+import dotenv from "dotenv";
+dotenv.config(); 
+
+
+
+
 
 cloudinary.config({
-  cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
-  api_key:process.env.CLOUDINARY_API_KEY,
-  api_secret:process.env.CLOUDINARY_SECRET_KEY  
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
 });
+
 
 
 
 const storage = new CloudinaryStorage({
-    cloudinary:cloudinary,
-    params: "properties",
-    allowed_formats:["jpg", "jpeg", "png"]
+  cloudinary: cloudinary,
+  params: {
+    folder: "properties",
+    allowed_formats: ["jpg", "jpeg", "png"]
+  }
 });
 
-
-export const upload = multer({storage});
-
+export const upload = multer({ storage });
 
