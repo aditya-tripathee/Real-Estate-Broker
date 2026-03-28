a13ebf14933aa1d611ce19eed16060401e3bba02
aditya-tripathee <adityatripatheee@gmail.com>
2026-03-28 16:09:38 +0530

    auth middlewares

--- backend/middlewares/auth.middleware.js
+++ backend/middlewares/auth.middleware.js
@@ -1,26 +1,25 @@
 import jwt from "jsonwebtoken";
-import User from "../models/auth.model";
+import User from "../models/auth.model.js";
 
-
-export const authMiddleware = async(req,res,next)=>{
-    try {
-        const token = req.cookies.token;
-        if(!token){
-            return res.status(400).json({message:"Unauthorized"})
-        }
-
-        const decoded = jwt.verify(token,process.env.JWT_SECRET);
-        const user = await User.findById(decoded.userId).select("-password");
-        if(!user){
-            return res.status(404).json({message:"User not found"})
-        }
-        req.user = user;
-        next();
-        
-
-    } catch (error) {
-        console.error("Auth middleware error ",error);
-        return res.status(500).json({message:"Internal server error"});
+export const authMiddleware = async (req, res, next) => {
+  try {
+    const token = req.cookies?.token || req.headers.authorization?.split(" ")[1];
+    if (!token) {
+      return res.status(400).json({ message: "Unauthorized" });
     }
-}
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await User.findById(decoded.userId).select("-password");
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    req.user = user;
+    next();
+  } catch (error) {
+    console.error("Auth middleware error ", error);
+
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
+
 
