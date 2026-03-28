53ab8cb08463ab9b1685694edaf452b61bfacdca
aditya-tripathee <adityatripatheee@gmail.com>
2026-03-28 16:05:25 +0530

    database setup with mongoose

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,7 +5,7 @@ export const connectDB = async()=>{
         await mongoose.connect(process.env.MONGO_URI);
         console.log(`Database connected successfully!`);
     } catch (error) {
-        console.error(`Database error ${error}`)
+        console.error(`Database error ${error}`);
     }
 }
 
